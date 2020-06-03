e1ad9ca41677a60cef3a02af6a08fef455e83ff8
Kiranmayi Klc <user.name>
2020-06-03 14:30:57 -0700

    Add clarifai image basic feature

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, flash, render_template, request, redirect, url_for
 from config import config
 from models import RecipeModel
+import base64
+from clarifai.rest import ClarifaiApp
 
 
 app = Flask(__name__)
-app.config.from_object(config['prod'])
+app.config.from_object(config['dev'])
+
+print(config['dev'].API_KEY)
+image_app = ClarifaiApp(api_key=config['dev'].API_KEY)
+model = image_app.models.get(model_id="bd367be194cf45149e75f01d59f77ba7")
 
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         ingredients = []
@@ -26,10 +32,23 @@ def index():
         print(ingredients)
         recipes = RecipeModel().get_recipes(ingredients)
         return render_template('recipes.html', recipes=recipes)
-
     return render_template('index.html')
 
 
+@app.route('/upload-image', methods=['GET', 'POST'])
+def upload_image():
+    if request.method == "POST":
+        tags = []
+        if request.files:
+            image = request.files["image"].read()
+            print(image, type(image))
+            response = model.predict_by_bytes(image)
+            print(response)
+            ingredients = [x['name'] for x in response['outputs'][0]['data']['concepts']]
+            print(ingredients)
+            recipes = RecipeModel().get_recipes(ingredients)
+            return render_template('recipes.html', recipes=recipes)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -8,11 +8,14 @@ class DevConfig(Config):
     DEBUG = True
     DB_URI = os.environ.get('DATABASE_URI')
     DB_NAME = "all_recipes"
+    API_KEY = os.environ.get('CLARIFAI_API_KEY')
 
 class ProdConfig(Config):
     DEBUG = False
     DB_URI = os.environ.get('DATABASE_URI')
     DB_NAME = "all_recipes"
+    API_KEY = os.environ.get('CLARIFAI_API_KEY')
+
 
 
 config = {

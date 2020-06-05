91baff95fe4faaa37411edefa7ac44cc2823f113
Kiranmayi Klc <user.name>
2020-06-04 17:33:52 -0700

    Add add/remove fields functionality

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask, flash, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, flash,redirect, url_for
 from config import config
 from models import RecipeModel
-import base64
 from clarifai.rest import ClarifaiApp
+from flask_wtf import FlaskForm
+from wtforms import Form, FieldList, FormField, StringField
+from wtforms.validators import Length
+
+
+class IngForm(Form):
+    ingredient = StringField('Ingredient', validators=[Length(max=30, message="Length of text exceeds 30 characters")],
+                             render_kw={"placeholder": "Enter ingredient", "class":"col-sm-3 my-1"})
+
+
+class MainForm(FlaskForm):
+    ingredients = FieldList(
+        FormField(IngForm),
+        min_entries=1,
+        max_entries=100
+    )
 
 
 app = Flask(__name__)
@@ -15,30 +30,29 @@ model = image_app.models.get(model_id="bd367be194cf45149e75f01d59f77ba7")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    form = MainForm()
     if request.method == 'POST':
         ingredients = []
-        if request.form['ing1']:
-            ingredients.append(request.form['ing1'])
-        if request.form['ing2']:
-            ingredients.append(request.form['ing2'])
-        if request.form['ing3']:
-            ingredients.append(request.form['ing3'])
-        if request.form['ing4']:
-            ingredients.append(request.form['ing4'])
-        if request.form['ing5']:
-            ingredients.append(request.form['ing5'])
-        if request.form['ing6']:
-            ingredients.append(request.form['ing6'])
+        if form.validate_on_submit():
+            print(form.ingredients.data)
+            for ing in form.ingredients.data:
+                if ing['ingredient'] and ing['ingredient'] != '':
+                    ingredients.append(ing['ingredient'])
         print(ingredients)
+        if not ingredients:
+            flash('Enter atleast one ingredient')
+            return redirect(url_for('index'))
         recipes = RecipeModel().get_recipes(ingredients)
+        if not recipes:
+            flash('No matching recipes exist')
+            return render_template('recipes.html')
         return render_template('recipes.html', recipes=recipes)
-    return render_template('index.html')
+    return render_template('index.html', form=form)
 
 
 @app.route('/upload-image', methods=['GET', 'POST'])
 def upload_image():
     if request.method == "POST":
-        tags = []
         if request.files:
             image = request.files["image"].read()
             print(image, type(image))
@@ -46,8 +60,18 @@ def upload_image():
             print(response)
             ingredients = [x['name'] for x in response['outputs'][0]['data']['concepts']]
             print(ingredients)
-            recipes = RecipeModel().get_recipes(ingredients)
-            return render_template('recipes.html', recipes=recipes)
+            form = MainForm()
+            for ing in ingredients:
+                print(ing)
+                if ing:
+                    ing_form = IngForm()
+                    ing_form.ingredient  = ing
+                #print(ing_form.ingredient.data)
+                    form.ingredients.append_entry(ing_form)
+                print(form.ingredients.data)
+            #recipes = RecipeModel().get_recipes(ingredients)
+            #return render_template('recipes.html', recipes=recipes)
+            return render_template('index.html',form=form)
     return render_template('index.html')
 
 
